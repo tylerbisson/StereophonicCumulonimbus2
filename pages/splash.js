adbec4287c1668a0c4904e736ebbd10949b07964
Tyler <tjb459@nyu.edu>
2021-12-09 20:17:32 -0500

    Edit linter rules

---
+++ pages/splash.js
@@ -0,0 +1,28 @@
+// import { connect } from 'react-redusdfx';
+import React from 'react'
+// import { logout, login } from '../actions/session_actions';
+// import { openModal, closeModal } from '../actions/modal_actions';
+// import { fetchSplashRecordings } from '../actions/recordings_actions';
+// import RecordingItem from '../components/recordings/recording_item';
+// import { receiveActiveRecording } from '../actions/active_recording_actions';
+
+
+const Splash = () => (<span>WHAT THE</span>)
+
+// const mapStateToProps = ({ session, entities: { users, recordings }, ui }) => ({
+//         recordings,
+//         currentUser: users[session.id],
+//         activeRecording: ui.activeRecording
+//     });
+
+// const mapDispatchToProps = dispatch => ({
+//     logout: () => dispatch(logout()),
+//     openModal: modal => dispatch(openModal(modal)),
+//     closeModal: () => dispatch(closeModal()),
+//     demoLogin: demoUser => dispatch(login(demoUser)),
+//     fetchSplashRecordings: () => dispatch(fetchSplashRecordings()),
+//     receiveActiveRecording: args => dispatch(receiveActiveRecording(args))
+// });
+
+export default Splash;
+
