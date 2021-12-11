0cb37dafc8a73c801f16ed81885fbaa820fb1d28
Tyler <tjb459@nyu.edu>
2021-12-11 14:08:16 -0500

    Remove Material UI

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,7 @@
-import Grid from "@material-ui/core/Grid";
-import styles from '../styles/layout.module.css'
+import styles from '../styles/layout.module.css';
 
 export default function Layout({ children }) {
   return (
-    <Grid container>
-      <div className={styles.container}>{children}</div>
-    </Grid>
-    );
+    <div className={styles.container}>{children}</div>
+  );
 };
